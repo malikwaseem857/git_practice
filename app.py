12c66aac5a0d9f5f4e72a87fc66c9cdd67046772
Malik Waseem <malikwaseem3802@gmail.com>
2025-06-21 20:09:11 +0500

    adding src and tests floders

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
-def add(a,b,c):
+def add(a,b+c):
     return a+b+c
 
-def subt(a,b):
-    return a-b 
+def subt(a,b,d):
+    return a-b-d
 
-def mult(a*b):
+def mult(a,b):
     return a*b
+
+    def div(a,b)
+         if b != 0
+         return a/b
+         print("can not divide by zero")
